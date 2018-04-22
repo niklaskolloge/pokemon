e7db066e1d179e8e242d79edcc6f05a3f40a9276
Niklas Kolloge <niklas.kolloge@gmail.com>
2018-04-22 10:18:28 +0200

    added details page for pokemon info

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -28,7 +28,7 @@ class PokemonController extends Controller
         $entityManager->persist($pokemon);
         $entityManager->flush();
 
-        return $this->render('pages/index.html.twig', ['name' => $name, 'pokemon'=> $pokemon]);
+        return $this->render('pages/index.html.twig', ['name' => $name, 'pokemon' => $pokemon]);
     }
 
     /**
@@ -45,5 +45,33 @@ class PokemonController extends Controller
         return $this->render('pages/index.html.twig', ['name' => $name, 'pokemon' => $pokemon]);
     }
 
+    /**
+     * @Route(path="/details/{id}", name="pokemondetails")
+     * @return Response
+     */
+    public function details_action(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $pokemon = $entityManager->getRepository(Pokemon::class)->find($id);
+
+        if ($pokemon === null) {
+            try {
+                $apiResponse = json_decode(file_get_contents("http://pokeapi.co/api/v2/pokemon/$id"), true);
+
+                $pokemon = new Pokemon();
+                $pokemon->setId($apiResponse['id']);
+                $pokemon->setName($apiResponse['name']);
+                $pokemon->setImage($apiResponse['sprites']['front_default']);
+                $pokemon->setWeight($apiResponse['weight']);
+                $pokemon->setHeight($apiResponse['height']);
+
+                $entityManager->persist($pokemon);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                return new Response("No Pokemon found for this ID");
+            }
+        }
+
+        return $this->render('pages/details.html.twig', ['pokemon' => $pokemon]);
+    }
 
 }
